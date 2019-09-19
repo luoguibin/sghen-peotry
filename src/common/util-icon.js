a9f3fd0bc596fd4d3faf139f37894baba29fc512
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2019-09-19 22:55:15 +0800

    [*]修复头像地址路径

--- src/common/util-icon.js
+++ src/common/util-icon.js
@@ -2,6 +2,6 @@ import { baseUrl } from '@/api/config'
 
 export const resetUserIconUrl = function (userInfo) {
   userInfo.iconUrl = userInfo.iconUrl
-    ? baseUrl + userInfo.iconUrl.substr(1)
-    : './favicon.ico'
+    ? baseUrl + "/peotry" + userInfo.iconUrl.substr(1)
+    : '/peotry/favicon.ico'
 }
