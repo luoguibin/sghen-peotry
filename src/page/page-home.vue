d6f71d9401d59e706ed02e1ca74765def499d8d1
Sghen <945654877@qq.com>
2019-09-21 13:51:09 +0800

    [*]添加wiki链接

--- src/page/page-home.vue
+++ src/page/page-home.vue
@@ -1,33 +1,42 @@
 <template>
   <div class="page-home">
-    <el-button type="text" @click="onPagePeotryList">Sghen诗词</el-button>
+    <div>
+      <el-button type="text" @click="onPagePeotryList">Sghen诗词</el-button>
+      <el-button type="text" @click="onPageWiki">wiki大法好</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
-  name: 'page-home',
+  name: "page-home",
 
-  created () {
-    this.showBack(false)
+  created() {
+    this.showBack(false);
   },
 
   methods: {
-    onPagePeotryList () {
-      this.$router.push({ name: 'peotry-list' })
+    onPagePeotryList() {
+      this.$router.push({ name: "peotry-list" });
+    },
+
+    onPageWiki() {
+      window.open("http://wiki.sghen.cn", '_blank')
     },
 
     ...mapActions({
-      showBack: 'showBack'
+      showBack: "showBack"
     })
   }
-}
+};
 </script>
 
 <style scoped>
 .page-home {
-
+  text-align: center;
+  min-height: 300px;
+  padding-top: 30px;
 }
 </style>
