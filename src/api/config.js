28f0b21caf48b0cee147c521069c662540e5be31
Sghen <945654877@qq.com>
2019-12-15 14:47:55 +0800

    [*]还原api配置

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://sapi.sghen.cn' : 'http://localhost:8085'
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'http://sapi.sghen.cn' : 'http://localhost:8085'
